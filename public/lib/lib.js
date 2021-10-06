b7388b0fc2b7b3ad26cf56013c0374257c09c198
Matias Vazquez-Levi <matiasvlevi@gmail.com>
2021-10-05 22:54:38 -0400

    build

--- public/lib/lib.js
+++ public/lib/lib.js
@@ -1,4 +1,25 @@
 /**
+ * Multiply two numbers.
+ * @method myfunc
+ * @param a first factor
+ * @param b second factor
+ * 
+ */
+function myfunc(a, b) {
+  return a * b;
+}
+/**
+ * Multiply two numbers and add a bias.
+ * @method myfunc2
+ * @param a first factor
+ * @param b second factor
+ * @param c translate
+ * 
+ */
+function myfunc2(a, b, c) {
+  return a * b + c;
+}
+/**
  * p5js Canvas Setup
  */
 let wnx = window.innerWidth;
